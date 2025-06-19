7b1fc10deb0d8dce34783f37a8d357c1e3544c3a
Divier Casas <diviercasas26@gmail.com>
2025-06-19 16:47:26 -0500

    Actualizacion

--- 06-reactive-forms-app/src/app/auth/pages/registerPage/register-page.component.ts
+++ 06-reactive-forms-app/src/app/auth/pages/registerPage/register-page.component.ts
@@ -1,11 +1,28 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.component.html'
 })
 export class RegisterPageComponent {
 
+  formBuilder = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['name', Validators.required],
+    email: ['email', Validators.email],
+    username: ['username', [Validators.required, Validators.minLength(6)]],
+    password: ['password', [Validators.required, Validators.minLength(6)]],
+    password2: ['password2', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+
+  }
 }
--- 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,11 +1,31 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html'
 })
 export class SwitchesPageComponent {
 
+  private formBuilder = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.formBuilder.group({
+    gender: [, Validators.required], //El primer valor se deja vacio para q no quede seleccionado ningun radio buton
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
+
+  onSubmit() {
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }

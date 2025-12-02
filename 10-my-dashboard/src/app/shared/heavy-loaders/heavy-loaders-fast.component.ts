bb732f54ed939d759b231932d050a9819d49dd64
Divier Alexis Casas Poveda <casasda+GHCO@hitss.com>
2025-12-02 11:40:43 -0500

    Actualizacion

--- 10-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ 10-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'app-heavy-loaders-fast.component',
-  imports: [],
+  selector: 'app-heavy-loaders-fast',
+  imports: [CommonModule],
     template:`
+    <section [ngClass]="['w-full', cssClass()]">
+      <ng-content></ng-content>
+    </section>
   `
 })
 export class HeavyLoadersFastComponent {
 
+  cssClass = input.required<string>();
+
+  constructor() {
+    console.log('HeavyLoadersFastComponent creado');
+  }
 }
